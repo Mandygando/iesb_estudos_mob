25225e6fef53e1ad0b2b698072822c98bc9bcef7
Amanda Viória <amandavic7576@gmail.com>
2023-08-23 10:30:17 -0300

    Atividade 1 criando o componente MinMax

--- my-app/components/MinMax.js
+++ my-app/components/MinMax.js
@@ -1,15 +1,22 @@
-import React from 'react'
-import { Text } from 'react-native'
+import React from "react";
+import { Text, StyleSheet } from "react-native";
 
-
-const MinMax = (props) => {
-
+export default function MinMax(props) {
     const { min, max } = props;
-   console.warn(props);
 
-  return (
-    <Text>{max}</Text>
-  )
+    const maxValue = Math.max(min, max);
+
+    return (
+        <Text style={styles.text}>
+            O maior valor entre {min} e {max} é: {maxValue}
+        </Text>
+    );
 }
 
-export default MinMax
+const styles = StyleSheet.create({
+    text: {
+        color: "white",
+        fontSize: 16,
+        fontWeight: "bold",
+    },
+});
